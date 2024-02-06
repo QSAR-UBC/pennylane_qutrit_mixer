6f42e77fae98f307068d47d907966801f59d20b0
gabrielLydian <gabrielbottrill@gmail.com>
2024-02-06 15:44:21 -0800

    Added initial logic to sampling and stubs for test classes files added properly

---
+++ tests/devices/qutrit_mixed/test_qutrit_mixed_simulate.py
@@ -0,0 +1,49 @@
+class TestCurrentlyUnsupportedCases:
+    pass
+
+
+def test_custom_operation():
+    """Test execution works with a manually defined operator if it has a matrix."""
+    pass
+
+
+class TestStatePrepBase:
+    """Tests integration with various state prep methods."""
+
+    pass
+
+
+class TestBasicCircuit:
+    """Tests a basic circuit with one rx gate and two simple expectation values."""
+
+    pass
+
+
+class TestBroadcasting:
+    """Test that simulate works with broadcasted parameters"""
+
+    pass
+
+
+class TestPostselection:  # TODO, necessesary?
+    """Tests for applying projectors as operations."""
+
+    pass
+
+
+class TestDebugger:
+    """Tests that the debugger works for a simple circuit"""
+
+    pass
+
+
+class TestSampleMeasurements:
+    """Tests circuits with sample-based measurements"""
+
+    pass
+
+
+class TestOperatorArithmetic:
+    """TODO add docstring"""
+
+    pass
